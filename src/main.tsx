178d4561df3ea34244aa9eeae451d140576d348c
Sam Magura <srmagura@gmail.com>
2022-09-17 05:37:43 -0400

    Remove vite

--- src/main.tsx
+++ src/main.tsx
@@ -1,27 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-
 import styled from "@emotion/styled";
 
 type Props = {
-  fontWeight?: 'bold' | 'normal'
-}
+  fontWeight?: "bold" | "normal";
+};
 
-const StyledButton = styled('button')<Props>({
-  'background-color': '#aa00bb',
-  'border': 0,
-  color: "#fff",
-  cursor: 'pointer',
-  padding: '20px'
-},
-(props) => ({
-  'text-align': 'center',
-  'font-weight': props.fontWeight,
-}))
-export { StyledButton };
-
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <StyledButton>I AM A BUTTON</StyledButton>
-  </React.StrictMode>
-)
+export const StyledButton = styled("button")<Props>((props) => ({
+  "text-align": "center",
+  "font-weight": props.fontWeight,
+}));
